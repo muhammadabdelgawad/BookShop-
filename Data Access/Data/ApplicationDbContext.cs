a1bb33853a7f6879aaefc14ef450f0e8b8a0ff66
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-07-27 21:10:38 +0300

    update migrations and add products table

--- Data Access/Data/ApplicationDbContext.cs
+++ Data Access/Data/ApplicationDbContext.cs
@@ -12,7 +12,7 @@ namespace Data_Access.Data
 
         }
         public DbSet<Category> Categories { get; set; }
-        //public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating (ModelBuilder modelBuilder)
         {
---
+++ Data Access/Migrations/20250727180705_CreateProductsTabletest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data_Access.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateProductsTabletest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(250)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    BookPrice = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+           
+        }
+    }
+}
